eb274abbbcd60a5e7ecad2f47974d0ec23a9bff3
0bezynka <41165411+0bezynka@users.noreply.github.com>
2022-08-24 10:15:14 +0300

    Update auto_status.py

--- auto_status.py
+++ auto_status.py
@@ -11,5 +11,6 @@ text = ['text1','text2','text3'] # СПИСОК
 while True:
     r_text = (choice(text)) # СЛУЧАЙНЫЕ ТЕКСТ ИЗ СПИСКА
     SS = vk.method("status.set",{"text":r_text}) # УСТАНОВКА СТАТУСА
+    print("Статус обновлён!")
 
     time.sleep(270) # СОН 5 МИНУТ
