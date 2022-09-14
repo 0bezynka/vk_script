f90efc6e59a9d5dd544022785f8e0fa9d25849f4
0bezynka <41165411+0bezynka@users.noreply.github.com>
2022-09-14 16:45:24 +0200

    добавляет в ЧС

---
+++ banned_user.py
@@ -0,0 +1,14 @@
+import vk_api, time
+
+vk=vk_api.VkApi(token="TOKEN") # VK
+
+def banneduser():
+	chet = 0
+	FGR = vk.method("friends.getRequests",{"out":1}) # Исходящие заявки
+	for i in FGR['items']:
+		vk.method("account.ban",{"owner_id":i}) # Добавление в ЧС
+		chet += 1
+		print(f"[{chet}] Banned user")
+		time.sleep(2)
+
+banneduser()
