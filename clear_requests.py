20d49796fc6ffea9ac97e3bb92b037c7ed4e5336
0bezynka <41165411+0bezynka@users.noreply.github.com>
2024-07-16 17:12:50 +0300

    Update clear_requests.py

--- clear_requests.py
+++ clear_requests.py
@@ -1,14 +1,16 @@
 import vk_api, time
-
+"""
+Отменяет все исходящие заявки
+"""
 vk=vk_api.VkApi(token="TOKEN") # VK
 
-def banneduser():
+def clear_requests():
 	chet = 0
 	FGR = vk.method("friends.getRequests",{"out":1}) # Исходящие заявки
 	for i in FGR['items']:
-		vk.method("friends.delete",{"user_id":i}) # Добавление в ЧС
+		vk.method("friends.delete",{"user_id":i}) # Удаляет пользователя
 		chet += 1
-		print(f"[{chet}] Banned user")
-		time.sleep(2)
+		print(f"[{chet}] Отменил заявку")
+		time.sleep(1)
 
-banneduser()
+clear_requests()
