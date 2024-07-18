3a074b6c20026bce80386275e62df4b212a95be5
0bezynka <41165411+0bezynka@users.noreply.github.com>
2024-07-18 22:10:19 +0300

    Update online.py

--- online.py
+++ online.py
@@ -1,10 +1,10 @@
 import vk_api, time
 """
-(Вечный Онлайн) 
+Вечный онлайн
 """
 vk=vk_api.VkApi(token="ТОКЕН")
 
 while True:
     vk.method("account.setOnline")# Онлайн
     print("Онлайн обновлён!\nСон 5 минут")
-    time.sleep(240)# Время
+    time.sleep(300)# Время
