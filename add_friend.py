cdea8ced4071d6c5a16ee43fdedb6aa2bb615f73
0bezynka <41165411+0bezynka@users.noreply.github.com>
2024-07-18 22:08:34 +0300

    Update add_friend.py

--- add_friend.py
+++ add_friend.py
@@ -1,36 +1,20 @@
 import vk_api, time
 """
-ДОБАВЛЕНИЕ НОВЫХ ДРУЗЕЙ 
+Добавление новых друзей 
 """
 
-vk=vk_api.VkApi(token="TOKEN") # VK token
+vk=vk_api.VkApi(token="TOKEN")
 
-def friend_added():
-	Chet_FGR = 0 # счётчик №1
-	print("[ START ]")
-	
-	# Проверка входящих заявок 
-	FGR = vk.method("friends.getRequests")
-	
-	if FGR['count'] > 0: # Если заявки есть то ...
-		for items in FGR['items']:
-			FA = vk.method("friends.add",{"user_id":items}) # Одобрение заявки
-			Chet_FGR += 1 # счётчик №1
-		print(f"[{Chet_FGR}] Заявок одобрено!")
-	
-	# Отправка заявок
-	FGS = vk.method("friends.getSuggestions",{"filter":"mutual","count":50}) # Возможные друзья
-	spisok_FGS = (FGS['items'])
-	box1 = []
+def adding_friends():
+	chet = 1
+	# Возможные друзья - 50 чел
+	get_info = vk.method('friends.getSuggestions',{'count':50})
+	# Список айди
+	id_user = get_info['items']
+	for user in id_user:
+		# Отправка заявки
+		vk.method("friends.add",{"user_id":user['id']})
+		print(f"[ #{chet} ] Отправил заявку: {user['first_name']} {user['last_name']}")
+		chet += 1
 
-	for i in spisok_FGS:
-		box1.append(i['id'])# ПОПЛНЯЕТ СПИСОК
-
-	chet = 0 # счётчик №2
-	for add in box1:
-		chet += 1 # счётчик №2
-		FGS = vk.method("friends.add",{"user_id":add})# ДОБОВЛЕНИЕ ДР
-		UG= vk.method("users.get",{"user_ids":add})# ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ
-		
-		print(f"[{chet}] Заявка отправлена - {UG[0]['first_name']} {UG[0]['last_name']}")
-		time.sleep(5)
+		time.sleep(2)
